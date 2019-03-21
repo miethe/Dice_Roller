7787182fa69e6491cb52dd09bb22dc898fc31378
Nick Miethe <6800980+miethe@users.noreply.github.com>
2019-03-21 08:02:14 -0400

    Create dice_roller.py
    Moving this to its own repo from the dnd character creator

---
+++ dice_roller.py
@@ -0,0 +1,32 @@
+import random
+
+class Dice_Roller:
+
+    def roll_n_d_x(self, n_count, x_die_size):
+        rolls = []
+        for _ in range(n_count):
+            #randrange is [x,y)
+            rolls.append(random.randrange(1,x_die_size+1))
+        return rolls
+
+    def drop_low_and_tally(self, rolls):
+        if not rolls:
+            return rolls
+        rolls.sort()
+        return sum(rolls[1:])
+
+    def roll_ndx_y_times(self, times_rolled, die_size, total_iterations, drop_lowest = False):
+        total_rolls = []
+        if times_rolled is 0 or die_size is 0 or total_iterations is 0:
+            return total_rolls
+        for _ in range(total_iterations):
+            rolls = self.roll_n_d_x(times_rolled, die_size)
+            total_rolls.append(self.drop_low_and_tally(rolls))
+        if drop_lowest:
+            total_rolls.sort()
+            return total_rolls[1:]
+        else:
+            return total_rolls
+
+def _average(lst):
+    return sum(lst) / len(lst)
