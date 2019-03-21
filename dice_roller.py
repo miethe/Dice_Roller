3488d3aed7cc430bc6fc874b7c0b68ee8ccf38a1
Nick Miethe <6800980+miethe@users.noreply.github.com>
2019-03-21 08:05:24 -0400

    Update dice_roller.py
    cleaning up and extracting another function for the sake of completeness.

--- dice_roller.py
+++ dice_roller.py
@@ -8,20 +8,25 @@ class Dice_Roller:
             #randrange is [x,y)
             rolls.append(random.randrange(1,x_die_size+1))
         return rolls
-
-    def drop_low_and_tally(self, rolls):
+    
+    def drop_lowest(self, rolls):
         if not rolls:
             return rolls
         rolls.sort()
-        return sum(rolls[1:])
+        return rolls[1:]
+    
+    def drop_low_and_tally(self, rolls):
+        return sum(drop_lowest(rolls))
 
     def roll_ndx_y_times(self, times_rolled, die_size, total_iterations, drop_lowest = False):
         total_rolls = []
         if times_rolled is 0 or die_size is 0 or total_iterations is 0:
             return total_rolls
+        
         for _ in range(total_iterations):
             rolls = self.roll_n_d_x(times_rolled, die_size)
             total_rolls.append(self.drop_low_and_tally(rolls))
+            
         if drop_lowest:
             total_rolls.sort()
             return total_rolls[1:]
